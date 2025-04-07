7d59ec75739b6b9fcb76223ea885ec5377efd685
crist <c.gallegos09@ufromail.cl>
2025-04-07 16:24:40 -0400

    Clase ComputadorEscritorio

--- src/main/java/org/example/ComputadorEscritorio.java
+++ src/main/java/org/example/ComputadorEscritorio.java
@@ -1,13 +1,16 @@
 package org.example;
-
 public class ComputadorEscritorio extends DispositivoTecnologico {
     private String tarjetaVideo;
     private String fuentePoder;
     private String chasis;
     private Pantalla pantalla;
 
-    public ComputadorEscritorio(String marca, String modelo, String procesador, String memoriaRAM, String memoriaAlmacenamiento, int añoFabricacion, double precio, int stock, String tarjetaVideo, String fuentePoder, String chasis, Pantalla pantalla) {
-        super(marca, modelo, procesador, memoriaRAM, memoriaAlmacenamiento, añoFabricacion, precio, stock);
+    public ComputadorEscritorio() {}
+
+    public ComputadorEscritorio(String marca, int ram, int almacenamiento, String procesador, String modelo,
+                                int anioFabricacion, double precio, int stock,
+                                String tarjetaVideo, String fuentePoder, String chasis, Pantalla pantalla) {
+        super(marca, ram, almacenamiento, procesador, modelo, anioFabricacion, precio, stock);
         this.tarjetaVideo = tarjetaVideo;
         this.fuentePoder = fuentePoder;
         this.chasis = chasis;
@@ -15,44 +18,9 @@ public class ComputadorEscritorio extends DispositivoTecnologico {
     }
 
     @Override
-    public String getTipo() {
-        return "Computador de Escritorio";
-    }
-
-    public String getTarjetaVideo() {
-        return tarjetaVideo;
-    }
-
-    public void setTarjetaVideo(String tarjetaVideo) {
-        this.tarjetaVideo = tarjetaVideo;
-    }
-
-    public String getFuentePoder() {
-        return fuentePoder;
-    }
-
-    public void setFuentePoder(String fuentePoder) {
-        this.fuentePoder = fuentePoder;
-    }
-
-    public String getChasis() {
-        return chasis;
-    }
-
-    public void setChasis(String chasis) {
-        this.chasis = chasis;
-    }
-
-    public Pantalla getPantalla() {
-        return pantalla;
-    }
-
-    public void setPantalla(Pantalla pantalla) {
-        this.pantalla = pantalla;
-    }
-
-    @Override
-    public String toString() {
-        return super.toString() + " | Tarjeta de Video: " + tarjetaVideo + " | Fuente de Poder: " + fuentePoder + " | Chasis: " + chasis + " | Pantalla: " + pantalla;
+    public String especificaciones() {
+        return "Computador de escritorio: " + marca + " " + modelo + "\nRAM: " + ram + "GB, Almacenamiento: " +
+                almacenamiento + "GB, Procesador: " + procesador + "\nTarjeta de video: " + tarjetaVideo +
+                ", Fuente: " + fuentePoder + ", Chasis: " + chasis + "\nPantalla: " + pantalla.toString();
     }
 }
