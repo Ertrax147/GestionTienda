4cb1f2e30240f9778a2277de785f100bef258ebf
crist <c.gallegos09@ufromail.cl>
2025-04-07 16:26:07 -0400

    getters y setters

--- src/main/java/org/example/Pantalla.java
+++ src/main/java/org/example/Pantalla.java
@@ -14,6 +14,30 @@ public class Pantalla {
         this.anio = anio;
     }
 
+    public String getMarca() {
+        return marca;
+    }
+
+    public void setMarca(String marca) {
+        this.marca = marca;
+    }
+
+    public String getModelo() {
+        return modelo;
+    }
+
+    public void setModelo(String modelo) {
+        this.modelo = modelo;
+    }
+
+    public int getAnio() {
+        return anio;
+    }
+
+    public void setAnio(int anio) {
+        this.anio = anio;
+    }
+
     @Override
     public String toString() {
         return marca + " " + modelo + " (" + anio + ")";
